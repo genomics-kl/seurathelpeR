594cb3da2fe3b0e4257a6902fdef2785f54119c5
Kin Lau <kin.lau@vai.org>
2019-08-18 14:20:31 -0400

    Initial commit.

---
+++ R/zzz.R
@@ -0,0 +1,20 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("seurathelper contains scripts for working with the Seurat package.")
+}
+
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.devtools <- list(
+    devtools.path = "~/R-dev",
+    devtools.install.args = "",
+    devtools.name = "Kin Lau",
+    devtools.desc.author = "Kin Lau <kin.lau@vai.org> [aut, cre]",
+    devtools.desc.license = "What license is it under?",
+    devtools.desc.suggests = NULL,
+    devtools.desc = list()
+  )
+  toset <- !(names(op.devtools) %in% names(op))
+  if(any(toset)) options(op.devtools[toset])
+
+  invisible()
+}
