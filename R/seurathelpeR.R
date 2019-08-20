57da939b529efcea1cb6010fcdaa0b9d2c0c4bef
Kin Lau <kin.lau@vai.org>
2019-08-19 20:47:38 -0400

    Fix minor documentation typo.

--- R/seurathelpeR.R
+++ R/seurathelpeR.R
@@ -1,6 +1,6 @@
 #' seurathelpeR: A package with convenience functions for working with Seurat v3 objects.
 #'
-#' Simple convenience functions for working wit Seurat objects.
+#' Simple convenience functions for working with Seurat objects.
 #'
 #' @section seurathelpeR functions:
 #' count_cells
